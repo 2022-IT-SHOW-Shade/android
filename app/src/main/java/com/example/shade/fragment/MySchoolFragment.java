5216536cdb62bdbc13be2d3685e14e658e9e5d97
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-05-17 13:16:35 +0900

    📝 :: 메인 하단바 기능 구현

---
+++ app/src/main/java/com/example/shade/fragment/MySchoolFragment.java
@@ -0,0 +1,17 @@
+package com.example.shade.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.shade.R;
+
+public class MySchoolFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_my_school, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/shade/fragment/TimeLineFragment.java
@@ -0,0 +1,19 @@
+package com.example.shade.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.shade.R;
+
+public class TimeLineFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_timeline, container, false);
+    }
+
+}
