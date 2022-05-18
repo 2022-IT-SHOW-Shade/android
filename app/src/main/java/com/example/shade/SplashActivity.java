bdb0afd9cd577acf0f5122851b78ae3aa5078307
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-05-18 22:22:35 +0900

    📑 :: splash 애니메이션 추가 및 재정리

--- app/src/main/java/com/example/shade/SplashActivity.java
+++ app/src/main/java/com/example/shade/SplashActivity.java
@@ -2,16 +2,25 @@ package com.example.shade;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    ImageView icon;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        icon = findViewById(R.id.splash_icon);
+        // Animation anim = AnimationUtils.loadAnimation(this, R.anim.spin); 추후에 스플래시 애니메이션 넣기
+        // icon.setAnimation(anim);
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
 }
+
