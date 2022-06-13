ec589ac8571d03c3c603af5c19ff816903fbbf59
U-and-Me <yunasun0920@gmail.com>
2022-06-13 12:05:28 +0900

    글 자세히 보기 구현

--- app/src/main/java/com/example/shade/fragment/MySchoolFragment.java
+++ app/src/main/java/com/example/shade/fragment/MySchoolFragment.java
@@ -11,7 +11,10 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.shade.R;
 import com.example.shade.TimeLineAdapter;
@@ -28,7 +31,7 @@ import java.util.ArrayList;
 
 public class MySchoolFragment extends Fragment {
 
-    ListView contents_school;
+    RecyclerView contents_school;
 
     TimeLineAdapter adapter;
     ArrayList<Post> respone = new ArrayList<>();
@@ -76,18 +79,20 @@ public class MySchoolFragment extends Fragment {
 
                     if(my_school.equals(post.getSchool())) {
                         // 각각의 데이터
+                        String num = post.getPost_num();
                         String title = post.getTitle();
-                        String content = post.getContent();
+                        String content = post.getContent().split("\n")[0];
                         String nickname = post.getUser_nick();
                         long like_cnt = post.getLike_cnt();
                         long chat_cnt = post.getComment_cnt();
 
-                        respone.add(new Post(title, content, nickname, like_cnt, chat_cnt));
+                        respone.add(new Post(num, title, content, nickname, like_cnt, chat_cnt));
                     }
 
                     // 리스트뷰 띄우기
-                    adapter = new TimeLineAdapter(respone);
+                    adapter = new TimeLineAdapter(respone, getContext());
                     contents_school.setAdapter(adapter);
+                    contents_school.setLayoutManager(new LinearLayoutManager(getContext()));
                 }
 
             }
